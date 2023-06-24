2c9f0700fe8e65106cda88aef65b61d0db6bed75
Camilo <camilocubillostorres@gmail.com>
2023-06-23 19:38:41 -0500

    Actualizar uso panda y solicitudesweb

--- app/main.py
+++ app/main.py
@@ -1,23 +1,37 @@
 import modules2
 import readcsv 
 import charts
-
+import pandas as pd
 def run():
+  
+  #sin pandas
+  '''
    data = readcsv.read_csv('./data.csv')
    data = list(filter(lambda item : item['Continent'] == 'South America',data))
    countries = list(map(lambda x: x['Country/Territory'], data))
    percentages = list(map(lambda x: x['World Population Percentage'], data))
    charts.generate_pie_chart(countries, percentages)
-   country=input('Digite el pais=>')
-   result = modules2.population_by_country(data,country)
-   if len(result) > 0:
+   '''
+   #Con Pandas 
+  df = pd.read_csv('data.csv')
+  df =df[df['Continent'] == 'Africa']
+   
+  countries=df['Country/Territory'].values
+  percentages=df['World Population Percentage'].values
+  charts.generate_pie_chart(countries, percentages)
+  
+  data = readcsv.read_csv('./data.csv')
+  country=input('Digite el pais=>')
+  result = modules2.population_by_country(data,country)
+  
+  if len(result) > 0:
      country=result[0]
-     print(country['Country/Territory'])
+     pais=country['Country/Territory']
      labels, values = modules2.get_population(country)
      #labels1=list(labels)
      #values1=list(values)
-     charts.generate_bar_char('Colombia',labels, values)
-   
+     charts.generate_bar_char(pais,labels, values)
+  
   
      
 
